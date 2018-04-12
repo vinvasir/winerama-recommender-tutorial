27f49ae0d2fccf1b49edc7fc5c4887f43c46ef9c
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-04-12 14:10:04 -0400

    get crud to work with adding reviews. NB we wrap the average_rating map() call in a list() call so that it can be used in our templates

--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Review, Wine
+from .forms import ReviewForm
+import datetime
 
 # Create your views here.
 def review_list(request):
@@ -14,9 +18,31 @@ def review_detail(request, review_id):
 
 def wine_list(request):
   wine_list = Wine.objects.order_by('-name')
-  context = {'wine_list', wine_list}
+  context = {'wine_list': wine_list}
   return render(request, 'reviews/wine_list.html', context)
 
 def wine_detail(request, wine_id):
   wine = get_object_or_404(Wine, pk=wine_id)
+  form = ReviewForm()
+  context = {'wine': wine, 'form': form}
   return render(request, 'reviews/wine_detail.html', context)
+
+def add_review(request, wine_id):
+  wine = get_object_or_404(Wine, pk=wine_id)
+  form = ReviewForm(request.POST)
+  if form.is_valid():
+    rating = form.cleaned_data['rating']
+    comment = form.cleaned_data['comment']
+    user_name = form.cleaned_data['user_name']
+
+    review = Review()
+    review.wine = wine
+    review.user_name = user_name
+    review.rating = rating
+    review.comment = comment
+    review.pub_date = datetime.datetime.now()
+    review.save()
+
+    return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+
+  return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
--- winerama/winerama/urls.py
+++ winerama/winerama/urls.py
@@ -18,6 +18,6 @@ from django.urls import include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^reviews/', include('reviews.urls')),
+    url(r'^reviews/', include(('reviews.urls', 'reviews'), namespace="reviews")),
     url(r'^admin/', admin.site.urls),
 ]
